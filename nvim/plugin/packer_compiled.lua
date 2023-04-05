de6c45a90c45bec2ba35fde9602645282484a325
dlacasse-cghy <124648118+dlacasse-cghy@users.noreply.github.com>
2023-04-04 20:43:02 -0400

    Add which-key

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -159,6 +159,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/dlacasse/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["which-key.nvim"] = {
+    loaded = true,
+    path = "/Users/dlacasse/.local/share/nvim/site/pack/packer/start/which-key.nvim",
+    url = "https://github.com/folke/which-key.nvim"
   }
 }
 
