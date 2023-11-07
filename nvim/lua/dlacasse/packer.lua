b0d6339085b04f446236d11c0be8c456f9f057b6
dlacasse-cghy <124648118+dlacasse-cghy@users.noreply.github.com>
2023-11-06 21:27:57 -0500

    Add Transparency Toggle

--- nvim/lua/dlacasse/packer.lua
+++ nvim/lua/dlacasse/packer.lua
@@ -1,12 +1,12 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 local function SetNvimRequirements()
-	-- disable netrw at the very start of your init.lua (strongly advised)
-	vim.g.loaded_netrw = 1
-	vim.g.loaded_netrwPlugin = 1
+    -- disable netrw at the very start of your init.lua (strongly advised)
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
-	-- set termguicolors to enable highlight groups
-	vim.opt.termguicolors = true
+    -- set termguicolors to enable highlight groups
+    vim.opt.termguicolors = true
 end
 SetNvimRequirements()
 
@@ -14,88 +14,89 @@ SetNvimRequirements()
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		-- or                            , branch = '0.1.x',
-		requires = { { 'nvim-lua/plenary.nvim' }, { "kdheepak/lazygit.nvim" } },
-		config = function()
-			require("telescope").load_extension("lazygit")
-		end,
-	}
-	use({
-		'rose-pine/neovim',
-		as = 'rose-pine',
-		config = function()
-			vim.cmd('colorscheme rose-pine')
-		end
-	})
-	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-	use('nvim-treesitter/playground')
-	use('nvim-lua/plenary.nvim')
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' }, -- Required
-			{
-				-- Optional
-				'williamboman/mason.nvim',
-				run = function()
-					pcall(vim.cmd, 'MasonUpdate')
-				end,
-			},
-			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' }, { "kdheepak/lazygit.nvim" } },
+        config = function()
+            require("telescope").load_extension("lazygit")
+        end,
+    }
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
+    use('nvim-lua/plenary.nvim')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {
+                -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' }, -- Required
-			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
-			{ 'L3MON4D3/LuaSnip' }, -- Required
-		}
-	}
-	use { 'nvim-tree/nvim-tree.lua',
-		requires = {
-			{ 'nvim-tree/nvim-web-devicons' }
-		}
-	}
-	use {
-		"folke/which-key.nvim",
-		config = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-			require("which-key").setup {
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			}
-		end
-	}
-	use { 'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons' }
-	use {
-		"NvChad/nvterm",
-		config = function()
-			require("nvterm").setup()
-		end,
-	}
-	use { 'vim-airline/vim-airline',
-		requires = {
-			{ 'vim-airline/vim-airline-themes' }
-		}
-	}
-	use { 'mg979/vim-visual-multi' }
-	use { 'kdheepak/lazygit.nvim' }
-	use {
-		"folke/zen-mode.nvim",
-		config = function()
-			require("zen-mode").setup {
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			}
-		end
-	}
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' }, -- Required
+        }
+    }
+    use { 'nvim-tree/nvim-tree.lua',
+        requires = {
+            { 'nvim-tree/nvim-web-devicons' }
+        }
+    }
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+    use { 'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons' }
+    use {
+        "NvChad/nvterm",
+        config = function()
+            require("nvterm").setup()
+        end,
+    }
+    use { 'vim-airline/vim-airline',
+        requires = {
+            { 'vim-airline/vim-airline-themes' }
+        }
+    }
+    use { 'mg979/vim-visual-multi' }
+    use { 'kdheepak/lazygit.nvim' }
+    use {
+        "folke/zen-mode.nvim",
+        config = function()
+            require("zen-mode").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+    use('xiyaowong/transparent.nvim')
 end)
